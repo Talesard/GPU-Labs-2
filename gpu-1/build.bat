d87d9582cfd5734261f89977bc1959a99083ab6b
Talesard <vetero4ekcs@mail.ru>
2022-03-01 18:57:22 +0300

    @echo off

--- gpu-1/build.bat
+++ gpu-1/build.bat
@@ -1,7 +1,9 @@
 :: запускать из x64 Native Tools Command Prompt for VS 2019
 :: 1 аргумент - файл
+@echo off
 set DPCPP_HOME=%USERPROFILE%\sycl_workspace
 set PATH=%DPCPP_HOME%\llvm\build\bin;%PATH%
 set LIB=%DPCPP_HOME%\llvm\build\lib;%LIB%
 
+@echo on
 clang++ -fsycl -fsycl-targets=nvptx64-nvidia-cuda,spir64_x86_64 %1 -o app.exe
